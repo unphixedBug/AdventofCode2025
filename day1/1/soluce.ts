7107c6f223771b9c95d6a5f1a43dccfce7c83748
philouLeF <delcroixphilippe73@gmail.com>
2025-12-01 23:48:00 +0100

    day 1 challenge 2

--- day1/1/soluce.ts
+++ day1/1/soluce.ts
@@ -9,21 +9,18 @@ for (const line of input) {
   const dir = line[0];
   const steps = Number(line.slice(1));
 
-  switch (dir) {
-    case "L":
-      pos = (pos - steps) % 100;
-      break;
-    case "R":
-      pos = (pos + steps) % 100;
-      break;
-  }
-
-  if (pos < 0) {
-    pos += 100;
-  }
-
-  if (pos === 0) {
-    zeroCount++;
+  for (let i = 0; i < steps; i++) {
+    switch (dir) {
+      case "L":
+        pos = (pos - 1 + 100) % 100;
+        break;
+      case "R":
+        pos = (pos + 1) % 100;
+        break;
+    }
+    if (pos === 0) {
+      zeroCount++;
+    }
   }
 }
 
