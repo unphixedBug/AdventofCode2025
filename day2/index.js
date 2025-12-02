b663f96cec10fd28869dae7c17fc1b0ce24a2af9
philouLeF <delcroixphilippe73@gmail.com>
2025-12-03 00:25:30 +0100

    day 2 challenge 1 done

--- day2/index.js
+++ day2/index.js
@@ -1,4 +1,24 @@
 const fs = require("fs");
 
 const input = fs.readFileSync("input.txt", "utf8").trim().split(",");
-console.log(input);
+
+let invalidIDs = 0;
+
+for (const range of input) {
+  const [rangeStart, rangeEnd] = range.split("-").map(Number);
+  for (let i = rangeStart; i <= rangeEnd; i++) {
+    if (i.toString().length % 2 === 0) {
+      const currentNumber = i.toString();
+      const middleIndex = currentNumber.length / 2;
+
+      const firstHalf = currentNumber.slice(0, middleIndex);
+      const secondHalf = currentNumber.slice(middleIndex);
+
+      if (firstHalf === secondHalf) {
+        invalidIDs += i;
+      }
+    }
+  }
+}
+
+console.log(invalidIDs);
